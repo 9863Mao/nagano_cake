db0279d83c569df9bb69997353afcd1476190236
9863mao <mao63black@gmail.com>
2022-04-15 13:22:54 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  devise_for :admin,skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customers,skip: [:passwords] ,controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
