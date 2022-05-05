1182b2058d595489e7fec9c9b653b3d35cd501fe
EC2 Default User <ec2-user@ip-172-31-39-162.ec2.internal>
2022-05-05 08:04:59 +0000

    [finish]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,10 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
+    @sum = 0
+    @cart_items.each do |cart_item|
+      @sum += cart_item.subtotal
+    end
 
   end
 
@@ -29,7 +33,7 @@ class Public::CartItemsController < ApplicationController
 
   def all_delete
     CartItem.destroy_all
-    redirect_to public_homes_top_path
+    redirect_to public_cart_items_path
   end
 
   def create
